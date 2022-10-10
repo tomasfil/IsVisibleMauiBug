83d1684f4f8d8977fc87fd811a240109101bd3dd
tomas.filip <tomas.filip@EPCEPC808>
2022-10-10 09:48:44 +0200

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,21 @@
+﻿namespace MauiApp1;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		var test = new NewContent1();
+
+
+        return builder.Build();
+	}
+}
